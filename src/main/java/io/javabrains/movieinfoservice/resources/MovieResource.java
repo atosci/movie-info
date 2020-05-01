3596ed506d2a5caf7918143a7a45797266efb431
tortugakawakibi <>
2020-05-01 13:59:14 +0200

    APi call naar movie database en microservice geeft data terug over movie.

--- src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
+++ src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -1,25 +1,29 @@
 package io.javabrains.movieinfoservice.resources;
 
+import com.google.gson.Gson;
 import io.javabrains.movieinfoservice.models.Movie;
-import io.javabrains.movieinfoservice.models.MovieSummary;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    @RequestMapping("/{movieTitle}")
+    public String getMovieInfo(@PathVariable("movieTitle") String movieTitle) {
 
-    @RequestMapping("/{movieId}")
-    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        MovieSummary movieSummary = new MovieSummary(movieId, "Home Alone " + movieId, "Thiefs and kid");
-        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        RestTemplate restTemplate = new RestTemplate();
+
+        String response = restTemplate.getForObject("http://www.omdbapi.com/?t=" + movieTitle + "&apikey=cc323c12", String.class);
+
+        Gson gson = new Gson();
+
+        Movie movie = gson.fromJson(response, Movie.class);
+
+        return gson.toJson(movie);
 
     }   
 
